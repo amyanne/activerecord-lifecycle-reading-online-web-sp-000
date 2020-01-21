5fb7c08e3d3ae9a49dace914941fd3068e7fa78e
Amy Westcott <amy.westcott@yahoo.com>
2020-01-21 00:05:16 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,5 @@
 class PostsController < ApplicationController
-  belongs_to :author
-  validate :is_title_case
  
-  # New Code!!
-  
-  before_validation :make_title_case
 	def index
 		@posts = Post.all
 	end
@@ -36,16 +31,6 @@ class PostsController < ApplicationController
 	  @post = Post.find(params[:id])
 	end
 	
-	private
- 
-  def is_title_case
-    if title.split.any?{|w|w[0].upcase != w[0]}
-      errors.add(:title, "Title must be in title case")
-    end
-  end
- 
-  def make_title_case
-    self.title = self.title.titlecase
-  end
+
 
 end
